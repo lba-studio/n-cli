ae0aa76d6a142e075bb32e34862a25cece27df6d
verzac <benjamin.tanone@gmail.com>
2024-01-30 22:34:16 +0700

    update description

--- cmd/cmd_run.go
+++ cmd/cmd_run.go
@@ -13,7 +13,9 @@ func NewRunCmd() *cobra.Command {
 	return &cobra.Command{
 		Use:     "run",
 		Aliases: []string{"r"},
+		Args:    cobra.MinimumNArgs(1),
 		Short:   "Runs arbitrary shell commands through n-cli.",
+		Long:    "Runs arbitrary shell commands. For example, `n-cli run echo Hello, world!` If you'd like to pass in flags to your shell command, use `n-cli run mycommand -- --flag1=true --flag2`.",
 		Run: func(cobraCmd *cobra.Command, args []string) {
 			command := args[0]
 			commandArgs := args[1:]
